c3353bb4259af6a23d7afeb11857c7a52e31bc51
JerinXplore <jerinjerin101325@gmail.com>
2025-11-02 00:52:02 +0600

    Replaced array/object with Map to track users/courses efficiently

--- compare-object-map.js
+++ compare-object-map.js
@@ -60,3 +60,31 @@ const user1 = {name: "Jerin"};
 userObj[user1] = "level1";
 console.log(userObj);//{ '[object Object]': 'level1' }//dekha jay j Object always string hoye jay tai alada user distinguish kora possible na..user1 ar user2 2 joner key same hoye jay...// { '[object Object]': 'level1' } ...Problem!!!
 
+/*map use korle*/
+const course5 = {name: "Programming Hero"};
+const course6 = {name: "level2"};
+const map2 = new Map();
+map2.set(course5, {courseId: "level1"});
+map2.set(course6, {courseId: "level1"});
+console.log(map2);
+
+// Map(2) {
+//   { name: 'Programming Hero' } => { courseId: 'level1' },
+//   { name: 'level2' } => { courseId: 'level1' }
+// }....ekhane course5 r 6 object key hishebe perfectly distinct thake..Map object reference k internally use kore jar fole course5 o course6 alada key hishebe store hoy..
+
+//cache /visit tracking e why useful?
+//1.karon protiti user ba course k key hishebe rakhte pare..eki user multiple visit korle key reuse hoy..easily update kora jay..
+//2. map average timeLookup provide kore..O(1)..onek fast
+// JavaScript e map internally hash table er moto kaj kore..ami jokhon key diye value set kori map.set(key, value),map key k hash kore ekta unique address toiri kore
+
+// Map approach
+ const map3 = new Map();
+map.set(user1, "level1");
+map.get(user1); // O(1)
+//ekhane map hash table use kore..hashing kore directly memory location ber kore
+
+// Array approach
+ const arr = [{id:1}, {id:2}, {id:3}];
+arr.find(u => u.id === 5000); // O(n) karon protiti element check korte hobe
+
