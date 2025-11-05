6cf0f0ea20216abcfe76360b2c01498b3f9ab04a
JerinXplore <jerinjerin101325@gmail.com>
2025-11-05 23:38:49 +0600

    Implemented LinkedList class with append and print methods

---
+++ append.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ append(value) {
+    const newNode = new Node(value);
+
+    //if the linked list is empty
+    if(this.head === null){
+        this.head = newNode ;
+        this.tail = newNode ;
+    } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+    this.length++;
+    return this;
+}
+  prepend(){
+
+  }
+
+  insert() {
+
+  }
+
+  remove() {
+
+  }
+
+  print() {
+    const arr = [] ;
+    let currentNode = this.head;
+
+    while (currentNode !== null){
+        arr.push(currentNode.value);
+        currentNode = currentNode.next;
+    }
+    console.log(arr.join("=>"), "=>null");
+  }
+}
+
+
+ 
